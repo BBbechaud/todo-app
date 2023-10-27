6ec28b89169fff7fca874b6f36d0cc9006a0267e
BBbechaud <132162306+BBbechaud@users.noreply.github.com>
2023-10-27 16:45:12 -0400

    Added task add, remove, and check functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
-let icon = document.getElementById('icon')
+const inputBox = document.querySelector('#input-box')
+const listContainer = document.querySelector('#list-container')
+// const btn = document.querySelector('.btn')
+
+document.querySelector('.btn').addEventListener('click', addTask)
+
+// Add task
+function addTask() {
+    if(inputBox.value === '') {
+        alert('Please add a task')
+    } else {
+        let li = document.createElement('li')
+        li.innerHTML = inputBox.value
+        listContainer.appendChild(li)
+        let span = document.createElement('span')
+        span.innerHTML = '\u00d7'
+        li.appendChild(span)
+    }
+        inputBox.value = ''
+}
+
+// Delete or check task
+listContainer.addEventListener('click', function(e) {
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked')
+    } else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove()
+    }
+}, false)
+
+// Darkmode toggle
+const icon = document.getElementById('icon')
 
 icon.onclick = function() {
     document.body.classList.toggle('darkmode')
